7d01ec34e330df1850815c2ce59d7ffe2a76d437
Yerik Yves Gavrie F. Talaboc <167282522+Gavrieee@users.noreply.github.com>
2025-05-26 20:59:10 +0800

    Update upload_image.php

--- core/upload_image.php
+++ core/upload_image.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once 'dbConfig.php'; // Adjust your DB connection
+require_once 'dbConfig.php';
 
 if (!isset($_SESSION['users_id'])) {
     echo json_encode(['success' => false, 'error' => 'Unauthorized']);
